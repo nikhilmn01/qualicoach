529cfac004bcd031f87e30d158cb512816c3d0bf
nikhilmn01 <88382860+nikhilmn01@users.noreply.github.com>
2021-10-15 18:54:49 +0530

    first commit

---
+++ mvnj/src/main/java/pageObjects/login.java
@@ -0,0 +1,31 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class login
+{
+	public WebDriver driver;
+	
+	public login(WebDriver driver) 
+	{
+		this.driver=driver;
+	}
+
+	public WebElement username()
+	{
+		return driver.findElement(By.id("username"));
+	}
+	
+	public WebElement pass()
+	{
+		return driver.findElement(By.id("password"));
+	}
+	
+	public WebElement ok()
+	{
+		return driver.findElement(By.id("loginbtn"));
+	}
+
+}
